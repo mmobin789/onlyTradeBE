42153295b5b76f0d9df75623b8352c2cfe189b03
Mobin Munir <mmobinbutt@gmail.com>
2025-04-20 01:50:53 +0500

    Update d user data in Mapper.kt for id type change.

--- src/main/kotlin/onlytrade/app/login/data/user/mapper/Mapper.kt
+++ src/main/kotlin/onlytrade/app/login/data/user/mapper/Mapper.kt
@@ -4,7 +4,7 @@ import onlytrade.app.login.data.user.dao.UserDAO
 import onlytrade.app.viewmodel.login.repository.data.db.User
 
 fun UserDAO.toModel(): User = User(
-    id = id.value,
+    id = id.value.toLong(),
     name = usersName,
     password = password,
     phone = phone,
